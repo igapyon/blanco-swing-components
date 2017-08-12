d5b889e832a93f6925c120a255a88efa7c46897c
Toshiki Iga <igapyon@gmail.com>
2017-08-13 08:15:37 +0900

    save

---
+++ src/main/java/blanco/swing/components/button/BlancoExitButton.java
@@ -0,0 +1,24 @@
+package blanco.swing.components.button;
+
+import java.awt.Component;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
+
+public class BlancoExitButton extends JButton {
+	private static final long serialVersionUID = 1L;
+
+	public void init() {
+		setText("Exit");
+		addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				final Component cmpnt = (Component) event.getSource();
+				final Window wndw = SwingUtilities.getWindowAncestor(cmpnt);
+				wndw.dispose();
+			}
+		});
+	}
+}
